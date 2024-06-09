0d97918ed6ddb3a3d618829f440946d13e213ae9
捋捉菌 <lzj2729033776@163.com>
2024-06-09 17:03:47 +0800

    main.xml changed

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -6,33 +6,25 @@ import android.os.Handler;
 import android.os.Message;
 import android.view.View;
 import android.view.Window;
+import android.view.WindowManager;
 import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
-import android.view.WindowManager;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.annotation.SuppressLint;
 
-
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button playBtton;
-    SeekBar start,end;
-    TextView startText,endText;
+    Button playButton;
+    SeekBar playLine, volume;
+    TextView startText, endText;
     MediaPlayer song;
     ImageView imageView;
     Animation animation;
-    int SongTotalTime;
+    int songTotalTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,119 +34,106 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        playBtton = findViewById(R.id.play);
+        playButton = findViewById(R.id.play);
         startText = findViewById(R.id.TextStart);
         endText = findViewById(R.id.TextEnd);
         imageView = findViewById(R.id.img);
-        animation = AnimationUtils.loadAnimation(this,R.anim.rotation);
+        animation = AnimationUtils.loadAnimation(this, R.anim.rotation);
 
-//        song
-        song = MediaPlayer.create(this,R.raw.song);
+        song = MediaPlayer.create(this, R.raw.song);
         song.setLooping(true);
         song.seekTo(0);
-        song.setVolume(0.5f,0.5f);
-        SongTotalTime = song.getDuration();
+        song.setVolume(0.5f, 0.5f);
+        songTotalTime = song.getDuration();
 
-        start = findViewById(R.id.PlayLine);
-        start.setMax(SongTotalTime);
-        start.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        playLine = findViewById(R.id.PlayLine);
+        playLine.setMax(songTotalTime);
+        playLine.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                if (b){
+                if (b) {
                     song.seekTo(i);
-                    start.setProgress(i);
+                    playLine.setProgress(i);
                 }
             }
 
             @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
+            public void onStartTrackingTouch(SeekBar seekBar) {}
 
             @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
+            public void onStopTrackingTouch(SeekBar seekBar) {}
         });
 
-        end = findViewById(R.id.volume);
-        end.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        volume = findViewById(R.id.volume);
+        volume.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                if (b){
-                    float volume = 1/100f;
-                    song.setVolume(volume,volume);
+                if (b) {
+                    // 计算音量百分比
+                    float volumeValue = i / 100.0f;
+                    // 设置音量
+                    song.setVolume(volumeValue, volumeValue);
                 }
             }
 
             @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
+            public void onStartTrackingTouch(SeekBar seekBar) {}
 
             @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
+            public void onStopTrackingTouch(SeekBar seekBar) {}
         });
 
         new Thread(new Runnable() {
             @Override
             public void run() {
-                while(song != null){
+                while (song != null) {
                     try {
                         Message message = new Message();
                         message.what = song.getCurrentPosition();
                         handler.sendMessage(message);
                         Thread.sleep(1000);
-                    }catch (InterruptedException ignored){
-
-                    }
+                    } catch (InterruptedException ignored) {}
                 }
             }
         }).start();
-
     }
 
-    @SuppressLint("HandleerLeak")
-    private Handler handler = new Handler(){
-        @SuppressLint("SetText18n")
+    private Handler handler = new Handler() {
         @Override
-        public void handleMessage(Message message){
-            int SeekBarPosition = message.what;
+        public void handleMessage(Message message) {
+            int seekBarPosition = message.what;
 
-//          update song seek bar
-            start.setProgress(SeekBarPosition);
+            // 更新歌曲进度条
+            playLine.setProgress(seekBarPosition);
 
-//          update labels
-            String Time = createTimeText(SeekBarPosition);
-            startText.setText(Time);
+            // 更新标签
+            String currentTime = createTimeText(seekBarPosition);
+            startText.setText(currentTime);
 
-            String remainingTime = createTimeText(SongTotalTime - SeekBarPosition);
+            String remainingTime = createTimeText(songTotalTime - seekBarPosition);
             endText.setText("-" + remainingTime);
-
         }
     };
 
-    public String createTimeText(int time){
+    public String createTimeText(int time) {
         String timeText;
         int min = time / 1000 / 60;
         int sec = time / 1000 % 60;
         timeText = min + ":";
-        if (sec<10)timeText += "0";
+        if (sec < 10) timeText += "0";
         timeText += sec;
         return timeText;
     }
 
-    public void PlayButton(View view){
-        if (!song.isPlaying()){
-            song.start();;
+    public void PlayButton(View view) {
+        if (!song.isPlaying()) {
+            song.start();
             imageView.startAnimation(animation);
-            playBtton.setBackgroundResource(R.drawable.baseline_pause_circle_24);
-        }else {
+            playButton.setBackgroundResource(R.drawable.baseline_pause_circle_24);
+        } else {
             song.pause();
             imageView.clearAnimation();
-            playBtton.setBackgroundResource(R.drawable.baseline_play_circle_24);
+            playButton.setBackgroundResource(R.drawable.baseline_play_circle_24);
         }
     }
 }
